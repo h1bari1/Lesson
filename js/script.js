0773a5e322c4041d2340c7fdc9b34839cebeefdf
h1bari <romaratmir@gmail.com>
2022-10-27 22:13:02 +0300

    slider

--- js/script.js
+++ js/script.js
@@ -4,3 +4,24 @@ burger.addEventListener('click', (e) => {
     burger.classList.toggle('active-burger')
     menuList.classList.toggle('list-active')
 })
+$('.pets').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+  });
